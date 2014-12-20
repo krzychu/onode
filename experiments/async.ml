d16dcb87a0bf1fcd81d6b1fa0730cd1890db70fb
krzychu <krzychuch@gmail.com>
2014-12-20 13:25:36 +0100

    implemented decent scheduler

--- experiments/async.ml
+++ experiments/async.ml
@@ -1,21 +1,70 @@
 module Scheduler = struct
     type file_descr = Unix.file_descr 
-    type action = (file_descr -> unit)
-    type event = Read | Write | Exception
 
-    type action_list = (file_descr, action) Hashtbl.t
-    type t = action_list * action_list * action_list
+    type event = 
+        | Read of file_descr
+        | Write of file_descr
+        | Exception of file_descr
+        | Shutdown
 
-    let create () : t = (Hashtbl.create 0, Hashtbl.create 0, Hashtbl.create 0)
+    type handler = (file_descr -> unit)
 
-    let schedule (sch : t) (fd : file_descr) (ev : event) (f : action) =
-        let (r, w, e) = sch in
-        let tab = match ev with
-            | Read -> r
-            | Write -> w
-            | Exception -> e
+    type t = {
+        actions : (event, handler) Hashtbl.t;
+        timeout : float;
+    } ;;
+
+    let create timeout : t = {
+        actions = Hashtbl.create 0;
+        timeout = timeout;
+    } ;;
+
+    let schedule (sch : t) (e : event) (h : handler) = 
+        Hashtbl.add sch.actions e h ;;
+
+    let shutdown_scheduled (sch : t) = 
+        Hashtbl.mem sch.actions Shutdown ;;
+
+    let split_io (sch : t) =
+        let rec aux event _ (r, w, e) = match event with
+            | Shutdown          -> (r, w, e)
+            | Read c            -> ((c::r), w, e)
+            | Write c           -> (r, (c::w), e)
+            | Exception c       -> (r, w, (c::e))
         in
-        Hashtbl.add tab fd f ;;
+        Hashtbl.fold aux sch.actions ([], [], []) ;;
+
+    let select (sch : t) = 
+        let r, w, e = split_io sch in
+        let ar, aw, ae = Unix.select r w e sch.timeout in
+        let mkread x = Read x in
+        let mkwrite x = Write x in
+        let mkexc x = Exception x in
+        [List.map mkread ar; List.map mkwrite aw; List.map mkexc ae] 
+            |> List.concat
+
+    let getfd e = match e with
+        | Shutdown -> Unix.stderr
+        | Read c -> c
+        | Write c -> c
+        | Exception c -> c
+            
+    let rec go (sch : t) =
+        let ev = select sch in
+        let prepare e = 
+            let fd = getfd e in
+            Hashtbl.find_all sch.actions e |> List.map (fun h -> (fd, h))
+        in
+        let to_fire = ev |> List.map prepare |> List.concat in
+        let fire (fd, h) = h fd in
+        let rec remove e = 
+            if Hashtbl.mem sch.actions e then 
+                Hashtbl.remove sch.actions e;
+                remove e;
+        in
+        ev |> List.iter remove ;
+        to_fire |> List.iter fire ;
+        go sch ;;
 end
 
 module Async = struct
@@ -25,13 +74,17 @@ module Async = struct
     let return (x : 't) : 't async = fun _ tcont -> tcont x
 
     let await_read (fd : Scheduler.file_descr) : Scheduler.file_descr async = fun sch fdcont ->
-        Scheduler.schedule sch fd Scheduler.Read fdcont
+        Scheduler.schedule sch (Scheduler.Read fd) fdcont
 
     let await_write (fd : Scheduler.file_descr) : Scheduler.file_descr async = fun sch fdcont ->
-        Scheduler.schedule sch fd Scheduler.Write fdcont
+        Scheduler.schedule sch (Scheduler.Write fd) fdcont
 
     let (>>=) (x : 't async) (f : 't -> 'r async) : 'r async = fun sch rcont -> 
         let tcont t = (f t) sch rcont in x sch tcont
 
     let run (sch : Scheduler.t) (t : unit async) = t sch (fun () -> ())
+end
+
+module AsyncStream = struct
+
 end
