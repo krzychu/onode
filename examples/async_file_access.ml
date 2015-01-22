13d47eb6c599053c0954e14719735062f951bf34
krzychu <krzychuch@gmail.com>
2015-01-22 19:06:08 +0100

    both reading and writing work fine

--- examples/async_file_access.ml
+++ examples/async_file_access.ml
@@ -1,19 +1,29 @@
 open AsyncStream ;;
 open Async;;
 
-let fd = Unix.openfile "examples/heart_of_darkness.txt" [Unix.O_RDONLY; Unix.O_NONBLOCK] 0 ;;
+let infd = Unix.openfile "examples/heart_of_darkness.txt" [Unix.O_RDONLY; Unix.O_NONBLOCK] 0 ;;
+let outfd = Unix.openfile "copy.txt" [Unix.O_WRONLY; Unix.O_NONBLOCK] 0 ;;
 
-let stream = In.create fd 1024 ;;
+let istream = In.create infd 1024 ;;
+let ostream = Out.create outfd 1024 ;;
 
 let line_length = 256
 
 let cat =
-    let rec aux line =
-        Printf.printf "%s\n" line; 
-        flush stdout;
-        In.read_line line_length stream >>= aux
+    let rec aux line = match line with
+        | In.Just line ->
+                Printf.printf "%s\n" line; 
+                flush stdout;
+                Out.write_all "hehe: " ostream >>= fun () ->
+                Out.write_all line ostream >>= fun () ->
+                Out.write_all "\n" ostream >>= fun () ->
+                In.read_line line_length istream >>= aux
+        | In.EOF ->
+                In.close istream >>= fun () -> 
+                Out.close ostream >>= fun () ->
+                shutdown
     in
-    In.read_line line_length stream >>= aux
+    In.read_line line_length istream >>= aux 
 ;;
 
 let sch = Scheduler.create 0.1 ;;
--- src/AsyncStream.ml
+++ src/AsyncStream.ml
@@ -1,6 +1,59 @@
 open Async ;;
 
+module Out = struct
+    type t = {
+        fd: Unix.file_descr;
+        flush_threshold: int;
+        buffer: Buffer.t;
+    };;
+
+    let create fd flush_threshold = {
+        fd = fd;
+        flush_threshold = flush_threshold;
+        buffer = Buffer.create (2 * flush_threshold)
+    };;
+
+    let rec send fd buf offset len = 
+        if len = 0
+            then return ()
+            else
+                await_write fd >>= fun fd ->
+                let num_written = Unix.write fd buf offset len in
+                send fd buf (offset + num_written) (len - num_written)
+    ;;
+
+    let flush (os : t) : unit async = 
+        let len = Buffer.length os.buffer in
+        let all = Buffer.to_bytes os.buffer in
+        Buffer.reset os.buffer;
+        send os.fd all 0 len
+    ;;
+
+    let close (os : t) : unit async = 
+        flush os >>= fun () ->
+        Unix.close os.fd;
+        return ()
+    ;;
+
+    let write (d : bytes) (offset : int) (len : int) (os : t) : unit async = 
+        Buffer.add_subbytes os.buffer d offset len; 
+        if Buffer.length os.buffer > os.flush_threshold 
+            then flush os
+            else return ()
+    ;;
+
+    let write_all (d : bytes) (os : t) : unit async = 
+        write d 0 (Bytes.length d) os
+    ;;
+end
+
 module In = struct
+
+    type optbytes = Just of bytes | EOF ;;
+
+    let return_optbytes x = return (Just x)
+    let return_optbuffer x = return_optbytes (Buffer.to_bytes x)
+
     type t = {
         fd: Unix.file_descr;
         chunk_size: int;
@@ -12,7 +65,12 @@ module In = struct
         chunk_size = chunk_size;
         queue = ByteQueue.create (2 * chunk_size);
     };;
-   
+
+    let close (os : t) : unit async = 
+        Unix.close os.fd;
+        return ()
+    ;;
+
     let read_more (is : t) : int async = 
         await_read is.fd >>= fun fd -> 
         let chunk = Bytes.create is.chunk_size in 
@@ -27,32 +85,31 @@ module In = struct
             else read_more is
     ;;
 
-    let read (requested_len : int) (is : t) : bytes async = 
+    let read (requested_len : int) (is : t) : optbytes async = 
         let out = Buffer.create requested_len in
         let rec aux remaining = 
             if remaining = 0
-                then return (Buffer.to_bytes out)
+                then return_optbuffer out
                 else read_if_needed remaining is >>= fun ar ->
-                    let n = min (ByteQueue.length is.queue) remaining in
-                    Buffer.add_bytes out (ByteQueue.pop n is.queue);
-                    aux (remaining - n)
+                    if ar = 0 then return EOF else
+                        let n = min (ByteQueue.length is.queue) remaining in
+                        Buffer.add_bytes out (ByteQueue.pop n is.queue);
+                        aux (remaining - n)
         in 
         aux requested_len;
     ;;
 
-    exception End_of_stream;;
-
-    let read_line (max_len : int) (is : t) : bytes async = 
+    let read_line (max_len : int) (is : t) : optbytes async = 
         let out = Buffer.create max_len in
         let rec aux prev =
             read_if_needed 1 is >>= fun k ->
-            if k = 0 then raise End_of_stream;
-            match (prev, ByteQueue.pop_one is.queue) with
-                | None     , '\n' -> return ""
-                | None     , curr -> aux (Some curr)
-                | Some '\r', '\n' -> return (Buffer.to_bytes out)
-                | Some prev, '\n' -> Buffer.add_char out prev; return (Buffer.to_bytes out)
-                | Some prev, curr -> Buffer.add_char out prev; aux (Some curr)
+            if k = 0 then return EOF else 
+                match (prev, ByteQueue.pop_one is.queue) with
+                    | None     , '\n' -> return_optbytes "" 
+                    | None     , curr -> aux (Some curr)
+                    | Some '\r', '\n' -> return_optbuffer out
+                    | Some prev, '\n' -> Buffer.add_char out prev; return_optbuffer out
+                    | Some prev, curr -> Buffer.add_char out prev; aux (Some curr)
         in
         aux None
      ;; 
